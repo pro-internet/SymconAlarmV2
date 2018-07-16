cd5f1b7ebe57819a7e7de92cc115802356a5b31a
PROINTERNET\Noah <n.wilhelm@prointernet.de>
2018-07-16 12:09:19 +0200

    no message

--- SymconAlarm/module.php
+++ SymconAlarm/module.php
@@ -86,7 +86,7 @@
 
                 } else if ($type == "alarm") {
 
-                    $rmessage = $rmessage . "<div style='color: red; font-size: 20px; padding: 15px; border: 2px solid red;'>" . $message . "</div><br />";
+                    $rmessage = "<div style='color: red; font-size: 20px; padding: 15px; border: 2px solid red;'>" . $message . $datum . "</div><br />";
 
                 }
 
